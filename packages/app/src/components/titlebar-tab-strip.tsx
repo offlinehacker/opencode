1638fca97e24dccbbb0ad1fbb80fed5894b24846
Jaka Hudoklin <jaka@x-truder.net>
2026-07-18 16:54:03 +0200

    Make tabs scroll and reasoning expandable

--- packages/app/src/components/titlebar-tab-strip.tsx
+++ packages/app/src/components/titlebar-tab-strip.tsx
@@ -88,7 +88,7 @@ function SessionTabSlot(props: {
       data-titlebar-tab-slot
       data-tab-key={props.id}
       data-active={props.active()}
-      class="relative flex w-56 min-w-7 max-w-56 flex-shrink"
+      class="relative flex w-48 shrink-0"
       classList={{ hidden: !session() && !missingSession() && !persisted()?.title }}
     >
       <TabNavItem
@@ -144,7 +144,7 @@ function DraftTabSlot(props: {
       data-titlebar-tab-slot
       data-tab-key={props.id}
       data-active={props.active()}
-      class="relative flex w-56 min-w-7 max-w-56 flex-shrink"
+      class="relative flex w-48 shrink-0"
     >
       <DraftTabItem
         ref={(el) => {
@@ -250,7 +250,7 @@ export function TitlebarTabStrip(props: {
             }
           }}
         >
-          <div data-titlebar-tab-list class="flex w-full min-w-0 flex-row items-center" ref={listRef}>
+          <div data-titlebar-tab-list class="flex w-max min-w-full flex-row items-center" ref={listRef}>
             <For each={props.tabs}>
               {(tab, index) => {
                 const id = tabKey(tab)
